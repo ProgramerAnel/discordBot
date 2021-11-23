0cfdef6db51f54afe8fe5741945d32a0ff63b633
Anel Hodžić <iml4203@Imel.Local>
2021-11-23 13:14:42 +0100

    Installed express to avoid HEROKU port error

--- bot.js
+++ bot.js
@@ -1,5 +1,18 @@
 require('dotenv').config()
 
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 const Discord = require('discord.js');
 const client = new Discord.Client()
 const sql = require("mssql");
@@ -27,7 +40,7 @@ client.on('ready', () => {
 client.on('message', msg => {
     if (msg.content.includes(PREFIX_COMMAND)) {
         value = msg.content.split(PREFIX_COMMAND)[1]
-
+        console.log('Validating request:', value)
         msg.reply('Validating for ' + value);
         (async function () {
             try {
